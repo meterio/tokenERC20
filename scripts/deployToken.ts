0cbd00d25b32c32cc3729ce33a2c20bf1c3d3062
崔棉大师 <1046166@qq.com>
2023-09-23 08:11:37 +0800

    update oft send

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -8,14 +8,18 @@ const main = async () => {
 
   const name = await input({
     message: "输入Token name:",
+    validate: (value = "") => value.length > 0 || "Pass a valid value",
   });
 
   const symbol = await input({
     message: "输入Token symbol:",
+    validate: (value = "") => value.length > 0 || "Pass a valid value",
   });
 
   const supply = await input({
     message: "输入初始供应量(单位:wei):",
+    default: "0",
+    validate: (value = "") => value.length > 0 || "Pass a valid value",
   });
 
   await deployContractV2(
