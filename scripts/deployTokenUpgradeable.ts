8821d393914ddd4dbbaacf2f016f9b9f4ffd1605
Simon.Z <zhanghan.simon@gmail.com>
2024-02-15 09:49:40 -0800

    give options to input deployed token impl in deployTokenUpgradeable

--- scripts/deployTokenUpgradeable.ts
+++ scripts/deployTokenUpgradeable.ts
@@ -12,43 +12,44 @@ const main = async () => {
   const network = await selectNetwork();
   let { override } = network;
 
+  const impl_answer = await select({
+    message: `Token Impl 合约:`,
+    choices: [
+      {
+        name: "部署合约",
+        value: "deployImpl",
+      },
+      {
+        name: "输入地址",
+        value: "inputImpl",
+      },
+    ],
+  });
+  let implAddress = "";
+
+  if (impl_answer == "deployImpl") {
+    const implementation = await deployContractV2(
+      ethers,
+      network,
+      "ERC20MintablePauseableUpgradeable",
+      [],
+      override
+    );
+    implAddress = await implementation.getAddress();
+  } else {
+    implAddress = await input({
+      message: "输入Token Impl地址:",
+      validate: (value = "") => isAddress(value) || "Pass a valid value",
+    });
+  }
+
   const pa = await input({
     message: "输入Proxy Admin地址:",
     validate: (value = "") => isAddress(value) || "Pass a valid value",
   });
 
-  const name = await input({
-    message: "输入Token name:",
-    validate: (value = "") => value.length > 0 || "Pass a valid value",
-  });
-
-  const symbol = await input({
-    message: "输入Token symbol:",
-    validate: (value = "") => value.length > 0 || "Pass a valid value",
-  });
-
-  const supply = await input({
-    message: "输入初始供应量(单位:wei):",
-    default: "0",
-    validate: (value = "") => value.length > 0 || "Pass a valid value",
-  });
-
-  const admin = await input({
-    message: "输入合约Admin:",
-    validate: (value = "") => isAddress(value) || "Pass a valid value",
-  });
-
-  const implementation = await deployContractV2(
-    ethers,
-    network,
-    "ERC20MintablePauseableUpgradeable",
-    [],
-    override
-  );
-  const implAddress = await implementation.getAddress();
-
   const proxy_answer = await select({
-    message: `选择${green(name)} 的${green("Proxy")}合约:`,
+    message: `选择Token 的${green("Proxy")}合约:`,
     choices: [
       {
         name: "部署合约",
@@ -62,6 +63,26 @@ const main = async () => {
   });
 
   if (proxy_answer == "deploy") {
+    const name = await input({
+      message: "输入Token name:",
+      validate: (value = "") => value.length > 0 || "Pass a valid value",
+    });
+
+    const symbol = await input({
+      message: "输入Token symbol:",
+      validate: (value = "") => value.length > 0 || "Pass a valid value",
+    });
+
+    const supply = await input({
+      message: "输入初始供应量(单位:wei):",
+      default: "0",
+      validate: (value = "") => value.length > 0 || "Pass a valid value",
+    });
+
+    const admin = await input({
+      message: "输入合约Admin:",
+      validate: (value = "") => isAddress(value) || "Pass a valid value",
+    });
     const factory = await ethers.getContractFactory(
       "ERC20MintablePauseableUpgradeable"
     );
@@ -76,7 +97,8 @@ const main = async () => {
       network,
       "SumerProxy",
       [implAddress, pa, data],
-      override
+      override,
+      `${symbol}-proxy`
     );
   } else if (proxy_answer == "update") {
     const proxyAddress = await input({
